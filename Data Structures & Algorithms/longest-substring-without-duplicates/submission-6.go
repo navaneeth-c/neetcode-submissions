0bcfc8d8fb5601ed1b707ccafa4d815e7ed10808
Navaneeth Chandrasekaran <navaneethcsiva@gmail.com>
2026-06-23 13:34:19 -0700

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.go
@@ -0,0 +1,17 @@
+func lengthOfLongestSubstring(s string) int {
+	// charSet := make(map[byte]bool)
+	var charSet [256]bool
+	l:=0
+	maxLen:=0
+
+	for r:=0 ; r < len(s); r++ {
+		for charSet[s[r]] {
+			charSet[s[l]] = false
+			l++
+		}
+
+		charSet[s[r]] = true
+		maxLen = max(maxLen, r - l + 1)
+	}
+	return maxLen
+}
