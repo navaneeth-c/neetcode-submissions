93caa23db503433f467363da7b36d67f389d1ebc
Navaneeth Chandrasekaran <navaneethcsiva@gmail.com>
2026-06-23 13:16:18 -0700

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.go
@@ -0,0 +1,21 @@
+func maxProfit(prices []int) int {
+	if len(prices) < 2 {
+		return 0
+	}
+	max_profit := 0
+	l := 0
+	r := 1
+	for l < r && r < len(prices){
+		if prices[l] > prices[r] {
+			l = r
+			r ++
+		} else {
+			curr := prices[r] - prices[l]
+			max_profit = max(max_profit, curr)
+			r++
+		}
+	}
+	return max_profit
+}
+
+
